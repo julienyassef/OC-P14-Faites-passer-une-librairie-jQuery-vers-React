25300621fbbe06a54ac44d7d1723c022e6401397
julien yassef <juyassef@gmail.com>
2024-08-16 12:56:47 +0200

    build

--- Version React/src/components/SortableHeaderTable/SortableHeaderTable.jsx
+++ Version React/src/components/SortableHeaderTable/SortableHeaderTable.jsx
@@ -9,13 +9,13 @@ import ArrowSvg from '../../assets/arrow.svg';
 
 const SortableHeaderTable = ({ title, columnKey, onSortChange }) => {
   return (
-    <th className='sortableHeader'>
+    <div className='sortableHeader'>
       <h2 className='sortableHeader__title'>{title}</h2>
       <div className='sortableHeader__arrow' >
         <img src={ArrowSvg} className='sortableHeader__arrow__ascending' onClick={() => onSortChange(columnKey, 'ascending')} alt="Arrow" />
       <img src={ArrowSvg} className='sortableHeader__arrow__descending' onClick={() => onSortChange(columnKey, 'descending')} alt="Arrow" />
       </div>
-    </th>
+    </div>
   );
 };
 
